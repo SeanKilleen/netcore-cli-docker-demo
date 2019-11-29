e6b6fa8471644950debf714ee6cffc7d2616555e
Sean Killeen <SeanKilleen@gmail.com>
2019-11-29 16:40:01 -0500

    dotnet new console --name MySolution.Console

---
+++ src/MySolution.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MySolution.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
